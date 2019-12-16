5b6910266bf01325ea80fa0378ad7e91612670d7
Carlos Baez <carlosbaez@Green-Planet.local>
2019-12-15 21:35:03 -0500

    created the welcome page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,17 +1,24 @@
 import React from "react";
-import Link from "react-router-dom";
-import Button from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { TextField, Grid, Button } from "@material-ui/core";
+import "../index.css";
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
-          className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="rick"
-        />
+
+        <div className="header-style">
+          <img
+            className="main-img"
+            src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            alt="rick"
+          />
+        </div>
+
+        <Link to="/characters"><Button>Characters</Button></Link>
+        <Link to="/characters"><Button>Search</Button></Link>
       </header>
     </section>
   );
